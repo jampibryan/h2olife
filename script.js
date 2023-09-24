0d14ba8d4322f4ac34e7fdfe6107a28b4a985423
JampiBryan <jampibryan@gmail.com>
2023-09-24 13:29:58 -0500

    site responsive

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 
+// Función para mostrar el menú responsivo
+let x = document.getElementById("menu-navegacion")
+
+function responsiveMenu() {
+    (x.className === "") ? x.className = "responsive" : x.className = ""
+}
+
+
+/* Función para cambiar la apariencia visual del menú de navegación y ocultarlo(menú) cuando el usuario selecciona una opción */
+
+function seleccionar(link) {
+    let opciones = document.querySelectorAll("#links a");
+    opciones.forEach(opcion => opcion.classList.remove("seleccionado"));
+    link.classList.add("seleccionado");
+
+    let x = document.getElementById("nav");
+    x.className = "";
+}
+
+
 const slider = document.querySelectorAll('.slider');
 const leftBtn = document.getElementById('left');
 const rightBtn = document.getElementById('right');
@@ -8,7 +28,7 @@ let activeSlide = 0;
 function updateSlide() {
     slider.forEach((slide) => slide.classList.remove('active'));
     slider[activeSlide].classList.add('active');
-    
+
 
     leftBtn.disabled = activeSlide === 0; //operación ternaria
     rightBtn.disabled = activeSlide === slider.length - 1;
